c2cdf9f97dfeb654fbce1efe7dddeabc2fe8456e
Juan <juandavidarguello@gmail.com>
2021-06-30 19:46:09 -0500

    .

--- Clases/Condicionales/untitled/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-	    byte x = 5;
-	    short y = 130;
-	    int z = y+5;
-
-    }
-}
--- Clases/Variables/untitled/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {    //Método main
-        String letra = "ñ";
-        int ñ = 5;
-        System.out.println(letra);
-    }
-}
