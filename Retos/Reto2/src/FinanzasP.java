6c272fd98681329fb3c89f349d396c24bb37fc5f
Juan <juandavidarguello@gmail.com>
2021-07-10 15:09:43 -0500

    Reto 2

--- Retos/Reto2/src/FinanzasP.java
+++ Retos/Reto2/src/FinanzasP.java
@@ -1,19 +1,66 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class FinanzasP {
     //Propiedades
     HashMap<String, Integer> carteraAhorros = new HashMap<String, Integer>();
     HashMap<String, Integer> gastosFijos = new HashMap<String, Integer>();
+    HashMap<String, Integer> gastos = new HashMap<String, Integer>();
 
-    public void registrarFijos(String nombre, int valor) {
+    public void utilidades () {
+        int utilidad = 0;
+        //Calcula el dinero disponible para inversion
 
+
+        System.out.println("Dispones de $" + utilidad + " para invertir en bolsa o criptomoneda.\n");
     }
 
-    public void crearEfectivo(int efectivo) {
+    public void movimientosF() {
+        //Resumen de movimientos financieros
+        int ingresos = 0;
+        int egresos = 0;
+        //---calculo de ingresos y egresos---
+
+        System.out.println("Resumen de movimientos: $" + ingresos + " de ingresos y $" + egresos + " de egresos.");
+    }
+
+    public void ConsultarCartera() {
+        int efectivo = 0;
+        int electronico = 0;
+        //---Tu desarrollo---
+
+
+        System.out.println("Dispones de $" + efectivo + " en efectivo y de $" + electronico + " en banco.\n");
+    }
+
+    public void gasto(Scanner input) {
+        System.out.print("Nombre del gasto: ");
+        String nombre = input.next();
+        System.out.print("Indica el valor: ");
+        int valor = input.nextInt();
+        gastos.put(nombre, valor);
+    }
+
+    public void registrarFijos(Scanner input) {
+        System.out.print("Ingresa el nombre del gasto fijo: ");
+        String nombre = input.next();
+        System.out.print("Indica el valor: ");
+        int valor = input.nextInt();
+
+        gastosFijos.put(nombre, valor);
+    }
+
+    public void Efectivo(Scanner input) {
+        System.out.print("Cuanto dinero tienes en efectivo?");
+        int efectivo = input.nextInt();
         carteraAhorros.put("Efectivo", efectivo);
+        System.out.println("Has registrado: $" + efectivo + " de dinero en efectivo.\n");
     }
 
-    public void crearEfElectronico(int dinero) {
-        carteraAhorros.put("Electronico", dinero);
+    public void EfElectronico(Scanner input) {
+        System.out.print("Cuanto dinero tienes en banco?");
+        int efectivo = input.nextInt();
+        carteraAhorros.put("Electronico", efectivo);
+        System.out.println("Has registrado: $" + efectivo + " de dinero en banco.\n");
     }
 }
--- Retos/Reto2/src/Main.java
+++ Retos/Reto2/src/Main.java
@@ -1,10 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+        public static void main(String[] args) {
+            //Herramientas
+            FinanzasP finanzas = new FinanzasP();
 
-    public static void main(String[] args) {
-        //Herramientas
-        Scanner input = new Scanner(System.in);
-        FinanzasP finanzas = new FinanzasP();
+            while (true) {
+                byte opcion = opcionesG();
+                switch (opcion) {
+                    case 1:
+                        finanzas.registrarFijos(input);
+                        break;
+                    case 2:
+                        finanzas.gasto(input);
+                        break;
+                    case 3:
+                        finanzas.Efectivo(input);
+                        break;
+                    case 4:
+                        finanzas.EfElectronico(input);
+                        break;
+                    case 5:
+                        finanzas.ConsultarCartera();
+                        break;
+                    case 6:
+                        finanzas.utilidades();
+                        break;
+                    case 7:
+                        finanzas.movimientosF();
+                        break;
+                }
+            }
+    }
+
+    public static byte opcionesG() {
+        byte opcion = 0;
+        while (opcion == 0) {
+            System.out.println("Menu de opciones:");
+            System.out.println("1. Registrar gasto fijo.");
+            System.out.println("2. Registrar gasto.");
+            System.out.println("3. Modificar dinero en efectivo.");
+            System.out.println("4. Modificar dinero en banco.");
+            System.out.println("5. Consulta de liquidez.");
+            System.out.println("6. Consulta de capital de inversion.");
+            System.out.println("7. Movimientos financieros.");
+            opcion = input.nextByte();
+            if (opcion > 7 || opcion < 0) {
+                opcion = (byte)0;
+            }
+        }
+        return opcion;
     }
 }
